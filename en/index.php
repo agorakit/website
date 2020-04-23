87645874f804221f676915fc6c1d0e0f65adc469
Philippe Jadin <philippe.jadin@gmail.com>
2020-04-23 21:48:34 +0200

    EN on par with FR

--- en/index.php
+++ en/index.php
@@ -1,37 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" href="https://app.agorakit.org/logo/favicon.png">
-
-    <title>Agorakit, open source groupware for collectives</title>
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" integrity="sha256-h20CPZ0QyXlBuAw7A+KluUYx/3pK+c7lYEpqLTlxjYQ=" crossorigin="anonymous" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/fontawesome.min.css" integrity="sha256-CuUPKpitgFmSNQuPDL5cEfPOOJT/+bwUlhfumDJ9CI4=" crossorigin="anonymous" />
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-
-    <link rel="stylesheet" href="../agorakit.css">
-
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-5064588-5"></script>
-    <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'UA-5064588-5');
-    </script>
-
-
-
-</head>
-
-<body>
-
-
+    <?php include('header.php') ?>
     <?php include('nav.php') ?>
 
 
@@ -205,15 +173,4 @@
                 </div>
             </footer>
 
-
-
-            <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-
-
-
-
-        </body>
-
-        </html>
+    <?php include('footer.php') ?>
--- en/services.php
+++ en/services.php
@@ -1,169 +1,176 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" href="https://app.agorakit.org/logo/favicon.png">
-
-    <title>Agorakit, open source groupware for collectives</title>
-
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" integrity="sha256-h20CPZ0QyXlBuAw7A+KluUYx/3pK+c7lYEpqLTlxjYQ=" crossorigin="anonymous" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/fontawesome.min.css" integrity="sha256-CuUPKpitgFmSNQuPDL5cEfPOOJT/+bwUlhfumDJ9CI4=" crossorigin="anonymous" />
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-
-    <link rel="stylesheet" href="../agorakit.css">
-
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-5064588-5"></script>
-    <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'UA-5064588-5');
-    </script>
+<?php include('header.php') ?>
+<?php include('nav.php') ?>
 
 
+<div class="container my-5">
 
-</head>
+    <h1 class="text-center mb-5" id="hebergement">Hosting</h1>
 
-<body>
+    <div class="card-deck mb-3 text-center align-self-stretch">
 
-
-    <?php include('nav.php') ?>
-
-
-
-
-    <div class="container my-5">
-        <h1 class="text-center mb-5">Hosting</h1>
-
-        <div class="card-deck mb-3 text-center align-self-stretch">
-
-
-            <div class="pricing-card card mb-4 shadow">
-                <div class="card-header">
-                    <h4 class="my-0 font-weight-normal">Name your price</h4>
-                </div>
-                <div class="card-body">
-                    <h1 class="card-title pricing-card-title">0 - 20 € <small class="text-muted">/ mo</small></h1>
-                    <ul class="list-unstyled mt-3 mb-4">
-
-                        <li>For tests and smaller groups</li>
-                        <li>No secret groups</li>
-                        <li>Maximum 3 mb per file</li>
-                        <li>Help & developper's group access</li>
-                        <li><a href="https://app.agorakit.org" target="_blank">https://app.agorakit.org</a></li>
-                    </ul>
-
-                </div>
-                <div class="card-footer">
-                    <a href="https://app.agorakit.org" type="button" class="btn btn-lg btn-block btn-outline-primary" target="_blank">Sign up for free</a>
-                </div>
+        <div class="pricing-card card mb-4 shadow">
+            <div class="card-header">
+                <h4 class="my-0 font-weight-normal">Small Agora</h4>
+            </div>
+            <div class="card-body">
+                <h1 class="card-title pricing-card-title">20€ <small class="text-muted">/ month</small></h1>
+                <ul class="list-unstyled mt-3 mb-4">
+                    <li>Your own private instance</li>
+                    <li>Managed hosting <br/>(upgrade and maintenance)</li>
+                    <li>Use your own (sub)domain</li>
+                    <li>10 groups</li>
+                    <li>50 users</li>
+                    <li>1Gb storage</li>
+                    <li>Email support</li>
+                    <li>Support group access</li>
+                </ul>
 
             </div>
 
+            <div class="card-footer">
+                      <a href="mailto:info@agorakit.org" type="button" class="btn btn-lg btn-block btn-primary">
+                          Contact us
+                      </a>
+            </div>
 
-            <div class="pricing-card card mb-4 shadow">
-                <div class="card-header">
-                    <h4 class="my-0 font-weight-normal">Standard instance</h4>
-                </div>
-                <div class="card-body">
-                    <h1 class="card-title pricing-card-title">40€ <small class="text-muted">/ mo</small></h1>
-                    <ul class="list-unstyled mt-3 mb-4">
-                        <li>Your own private instance</li>
-                        <li>Use your own domain or subdomain</li>
-                        <li>Unlimited users</li>
-                        <li>Large storage and files</li>
-                        <li>Priority email & phone support</li>
-                        <li>Help & developper's group access</li>
-                    </ul>
+        </div>
 
-                </div>
-
-                <div class="card-footer">
-                    <a href="mailto:info@agorakit.org" type="button" class="btn btn-lg btn-block btn-primary">Contact us</a>
-                </div>
+        <div class="pricing-card card mb-4 shadow">
+            <div class="card-header">
+                <h4 class="my-0 font-weight-normal">Medium Agora</h4>
+            </div>
+            <div class="card-body">
+                <h1 class="card-title pricing-card-title">40€ <small class="text-muted">/ mois</small></h1>
+                <ul class="list-unstyled mt-3 mb-4">
+                    <li>Your own private instance</li>
+                    <li>Managed hosting <br/>(upgrade and maintenance)</li>
+                    <li>Use your own (sub)domain</li>
+                    <li>25 groups</li>
+                    <li>250 users</li>
+                    <li>5Gb storage</li>
+                    <li>Email support</li>
+                    <li>Support group access</li>
+                </ul>
 
             </div>
 
+            <div class="card-footer">
+                <a href="mailto:info@agorakit.org" type="button" class="btn btn-lg btn-block btn-primary">
+                    Contact us
+                </a>
+            </div>
+
+        </div>
 
 
 
-            <div class="pricing-card card mb-4 shadow">
-                <div class="card-header">
-                    <h4 class="my-0 font-weight-normal">Supporter instance</h4>
-                </div>
-                <div class="card-body">
-                    <h1 class="card-title pricing-card-title">80€ <small class="text-muted">/ mo</small></h1>
-                    <ul class="list-unstyled mt-3 mb-4">
-                        <li>Everything you get in the standard plan</li>
-                        <li>You get to help us choose the features to add next</li>
-                        <li>You support the project by financing 2 hours of developement each month!</li>
-                    </ul>
 
-                </div>
-                <div class="card-footer">
-                    <a href="mailto:info@agorakit.org" type="button" class="btn btn-lg btn-block btn-primary">Contact us</a>
-                </div>
+        <div class="pricing-card card mb-4 shadow">
+            <div class="card-header">
+                <h4 class="my-0 font-weight-normal">Large Agora</h4>
+            </div>
+            <div class="card-body">
+                <h1 class="card-title pricing-card-title">80€ <small class="text-muted">/ mois</small></h1>
+                <ul class="list-unstyled mt-3 mb-4">
+                    <li>Your own private instance</li>
+                    <li>Managed hosting <br/>(upgrade and maintenance)</li>
+                    <li>Use your own (sub)domain</li>
+                    <li>Unlimited groups</li>
+                    <li>Unlimited users</li>
+                    <li>20Gb storage</li>
+                    <li>Email support</li>
+                    <li>Support group access</li>
+                    <li>Help the project by financing one hour of development each month</li>
+                </ul>
+
+            </div>
+            <div class="card-footer">
+                <a href="mailto:info@agorakit.org" type="button" class="btn btn-lg btn-block btn-primary">
+                    Contact us
+                </a>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+
+<div class="container my-5 justify-content-center">
+
+    <h1 class="text-center mb-5">Other options</h1>
+
+    <div class="card-deck mb-3 text-center align-self-stretch">
+
+
+        <div class="pricing-card card mb-4 shadow">
+            <div class="card-header">
+                <h4 class="my-0 font-weight-normal">Shared Agora</h4>
+            </div>
+            <div class="card-body">
+                <h1 class="card-title pricing-card-title">Name your price</h1>
+                <ul class="list-unstyled mt-3 mb-4">
+
+                    <li>Free for your tests and for non profit citizen groups</li>
+                    <li>Shared instance</li>
+                    <li>No secret groups</li>
+                    <li>3Mb max filesize</li>
+                    <li>Support group access</li>
+                    <li><a href="https://app.agorakit.org" target="_blank">https://app.agorakit.org</a></li>
+                </ul>
+
+            </div>
+            <div class="card-footer">
+                <a href="https://app.agorakit.org" type="button" class="btn btn-lg btn-block btn-outline-primary" target="_blank">
+                    Register for free
+                </a>
+            </div>
+
+        </div>
+
+        <div class="pricing-card card mb-4 shadow">
+            <div class="card-header">
+                <h4 class="my-0 font-weight-normal">Host-it yourself</h4>
+            </div>
+            <div class="card-body">
+                <h1 class="card-title pricing-card-title">0€ <small class="text-muted">/ month</small></h1>
+                <ul class="list-unstyled mt-3 mb-4">
+                    <li>No limits</li>
+                    <li>No freemium or "pro" edition</li>
+                    <li><strong>You handle everything yourself</strong></li>
+                    <li>GPL licence (open source)</li>
+                </ul>
+            </div>
+
+            <div class="card-footer">
+                <a href="https://github.com/agorakit/agorakit" type="button" class="btn btn-lg btn-block btn-outline-primary" target="_blank">Download</a>
+            </div>
+        </div>
+
+
+        <div class="pricing-card card mb-4 shadow">
+            <div class="card-header">
+                <h4 class="my-0 font-weight-normal">Development</h4>
+            </div>
+            <div class="card-body">
+                <h1 class="card-title pricing-card-title">60€ <small class="text-muted">/ hour</small></h1>
+                <ul class="list-unstyled mt-3 mb-4">
+                    <li>Customize Agorakit</li>
+                    <li>Get advices for your project</li>
+                    <li>Get a quote</li>
+                    <li>Everything is open source licenced</li>
+                </ul>
+            </div>
+            <div class="card-footer">
+                <a href="mailto:info@agorakit.org" type="button" class="btn btn-lg btn-block btn-primary">
+                    Contact us
+                </a>
             </div>
         </div>
 
     </div>
 
-
-    <div class="container my-5 d-flex justify-content-center">
-        <div class="col-9">
-
-            <h1 class="text-center mb-5">Host it yourself / Custom development</h1>
-
-            <div class="card-deck mb-3 text-center align-self-stretch">
+</div>
 
 
-                <div class="pricing-card card mb-4 shadow">
-                    <div class="card-header">
-                        <h4 class="my-0 font-weight-normal">Host it yourself</h4>
-                    </div>
-                    <div class="card-body">
-                        <h1 class="card-title pricing-card-title">0€ <small class="text-muted">/ mo</small></h1>
-                        <ul class="list-unstyled mt-3 mb-4">
-                            <li>No limits</li>
-                            <li>No freemium or commercial edition</li>
-                            <li><strong>You handle everything</strong></li>
-                            <li>GPL (open source) License</li>
-                            <li>Help & developper's group access</li>
-                        </ul>
-                    </div>
-
-                    <div class="card-footer">
-                        <a href="https://github.com/agorakit/agorakit" type="button" class="btn btn-lg btn-block btn-outline-primary" target="_blank">Get the source</a>
-                    </div>
-                </div>
-
-
-                <div class="pricing-card card mb-4 shadow">
-                    <div class="card-header">
-                        <h4 class="my-0 font-weight-normal">Custom developement</h4>
-                    </div>
-                    <div class="card-body">
-                        <h1 class="card-title pricing-card-title">40€ <small class="text-muted">/ hour</small></h1>
-                        <ul class="list-unstyled mt-3 mb-4">
-                            <li>Get advices</li>
-                            <li>Ask for a quote</li>
-                            <li>Everything is open source licensed</li>
-                        </ul>
-                    </div>
-                    <div class="card-footer">
-                        <a href="mailto:info@agorakit.org" type="button" class="btn btn-lg btn-block btn-primary">Contact us</a>
-                    </div>
-                </div>
-
-            </div>
-
-        </div>
-    </div>
-</body>
-
-</html>
+<?php include('footer.php') ?>
