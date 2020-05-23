0ffdbb2e6012850f96cae3780038ba35ac1463ff
Philippe Jadin <philippe.jadin@gmail.com>
2020-05-23 15:03:49 +0200

    ajout page merci

---
+++ fr/merci.php
@@ -0,0 +1,27 @@
+<?php include('header.php') ?>
+
+
+<?php include('nav.php') ?>
+
+
+
+<div class="container mt-3 mb-5">
+
+    <h1 class="text-center mb-4">Merci !</h1>
+
+    <h2 class="sub text-center mb-5">Votre soutien est très précieux et va permettre de faire évoluer Agorakit plus rapidement.</h2>
+
+
+    <p class="text-center">
+        Au nom de tous les utilisateurs.trices d'Agorakit, merci :-)
+    </p>
+
+    <p class="text-center">
+        N'hésitez pas à me contacter à <a href="mailto:info@agorakit.org">info@agorakit.org</a>
+    </p>
+
+</div>
+
+
+
+<?php include('footer.php') ?>
